8304979dc3274f64f91df7e3ed9e46743215533b
Eng-RonnieBM <ronniealberto@gmail.com>
2020-02-11 16:02:03 +0000

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,9 @@ void print_alphabet_x10(void)
 {
 int i;
 char n;
-for(i = 1; i <= 10; i++)
+for (i = 1; i <= 10; i++)
 {
-for(n = 'a'; n <= 'z' ; n++)
+for (n = 'a'; n <= 'z' ; n++)
 _putchar(n);
 _putchar('\n');
 }
