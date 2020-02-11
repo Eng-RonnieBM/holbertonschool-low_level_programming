285cc2fa5ecd6d7daae2fbfdb6700b8a97058758
Eng-RonnieBM <ronniealberto@gmail.com>
2020-02-11 14:05:52 +0000

    task0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,12 @@
+#include <stdlib.h>
 #include <stdio.h>
-/**
+/*
  * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-printf("Holberton\n");
+putchar("Holberton\n");
 return (0);
 }
