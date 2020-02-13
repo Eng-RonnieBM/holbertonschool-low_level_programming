a171627ea60c2ba6d830b3b5d409815a33b17ad2
Eng-RonnieBM <ronniealberto@gmail.com>
2020-02-13 18:18:56 -0500

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz-Buzz test.
+ *
+ * Return: Fizz-Buzz test.
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 1; i <= 100 ; i++)
+{
+if ((i % 3 == 0) && (i % 5 == 0))
+printf("FizzBuzz ");
+else if (i % 3 == 0)
+printf("Fizz ");
+else if (i % 5 == 0)
+printf("Buzz ");
+else
+printf("%d ", i);
+}
+printf("\n");
+}
