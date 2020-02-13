a1179926679ef78b891f088cf1f018b7a9b08cf6
Eng-RonnieBM <ronniealberto@gmail.com>
2020-02-13 15:04:31 -0500

    updating holberton.h

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,7 +1,4 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-#include <stdio.h>
+/* holberton.h */
 
 int _putchar(char c);
 int _isupper(int c);
@@ -14,7 +11,3 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
-
-
-#endif /* HOLBERTON_H */
