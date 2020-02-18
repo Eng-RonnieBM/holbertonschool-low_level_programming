020886bb265667a07eca702ecc31a39196fc7c3c
Eng-RonnieBM <ronniealberto@gmail.com>
2020-02-18 18:09:44 -0500

    creating task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 
 /**
  * puts2 - print every other character of a string.
- * @str: The string
+ * @str: The string.
  *
  * Return: void.
  */
@@ -10,10 +10,14 @@
 void puts2(char *str)
 {
 int n;
+char t;
 
-for (n = 0; str[n] != '\0'; n = n + 2)
+for (n = 0; str[n] != '\0'; n++)
 {
-_putchar(str[n]);
+if (n % 2 == 0)
+{
+t = str[n];
+putchar(t);
 }
-_putchar('\n');
+}
 }
