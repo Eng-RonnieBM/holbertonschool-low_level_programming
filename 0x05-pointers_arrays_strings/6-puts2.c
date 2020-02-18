782474b99944f4cee92b1ada8bc6f3ea5487a56f
Eng-RonnieBM <ronniealberto@gmail.com>
2020-02-18 17:51:10 -0500

    creating task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * puts2 - print every other character of a string.
+ * @str: The string
+ *
+ * Return: void.
+ */
+
+void puts2(char *str)
+{
+int n;
+
+for (n = 0; str[n] != '\0'; n = n + 2)
+{
+_putchar(str[n]);
+}
+_putchar('\n');
+}
