30d3a003222a0c68513c11c6b8c70ea40c4a40cf
Eng-RonnieBM <ronniealberto@gmail.com>
2020-02-20 17:43:25 -0500

    creating task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - a function that compares two strings.
+ * @s1: first pointer variable.
+ * @s2: second pointer variable.
+ * Return: comparision of two strings.
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i;
+int diff;
+
+for (i = 0; s1[i] != '\0' || s2[i] != '\0' ; i++)
+{
+
+if (s1[i] == s2[i])
+diff = 0;
+
+if (s1[i] != s2[i])
+diff = s1[i] - s2[i];
+break;
+}
+
+return (diff);
+}
