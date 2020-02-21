e01ca8a0cccdb21b226de84a3bfac685180acf54
Eng-RonnieBM <ronniealberto@gmail.com>
2020-02-20 21:36:11 -0500

    fixing task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,20 +6,28 @@
  * @s2: second pointer variable.
  * Return: comparision of two strings.
  */
+
 int _strcmp(char *s1, char *s2)
 {
 int i;
 int diff;
 
 for (i = 0; s1[i] != '\0' || s2[i] != '\0' ; i++)
+
 {
 
-if (s1[i] == s2[i])
-diff = 0;
-
 if (s1[i] != s2[i])
+{
 diff = s1[i] - s2[i];
 break;
+}
+
+if (s1[i] == s2[i])
+{
+diff = 0;
+break;
+}
+
 }
 
 return (diff);
