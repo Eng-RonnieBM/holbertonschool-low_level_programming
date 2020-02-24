6558cb17f8ebd9f11ae5917e4229203a79c4eeb4
Eng-RonnieBM <ronniealberto@gmail.com>
2020-02-24 17:35:04 -0500

    creating task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strchr - it returns an address of first character match.
+ * @s: char pointer variable.
+ * @c: char to find.
+ *
+ * Return: an address.
+ */
+
+char *_strchr(char *s, char c)
+{
+int counter;
+
+for (counter = 0; s[counter] != '\0'; counter++)
+{
+if (s[counter] == c)
+return (&s[counter]);
+}
+return ('\0');
+}
