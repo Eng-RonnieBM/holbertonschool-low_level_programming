f7395c4d755315532c4e6a03a4773d2dd661598e
Eng-RonnieBM <ronniealberto@gmail.com>
2020-02-25 22:59:37 -0500

    creating task5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _strstr - a function that locates a substring.
+ * @a: pointer to main string.
+ * @b: pointer to substring.
+ *
+ * Return: a pointer to the beginning of the located substring.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+while (*haystack != '\0')
+{
+char *start = haystack;
+char *match = needle;
+
+while (*match == *haystack && *match != '\0'
+&& *haystack != '\0')
+{
+haystack++;
+match++;
+}
+
+if (*match == '\0')
+return (start);
+
+haystack = start + 1;
+}
+return ('\0');
+}
