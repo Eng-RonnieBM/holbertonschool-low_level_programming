8660c54246b819bf33051ef8c6f627e5b522d971
Eng-RonnieBM <ronniealberto@gmail.com>
2020-03-05 16:56:00 -0500

    rewriting without strlen sizeof

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * string_nconcat - a function that concatenates two strings.
  * @s1: first string.
  * @s2: second string.
  * @n: amount of characters of s2 to be copyed.
- *
  * Return: a pointer.
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, counter;
-char *ptr;
+char *p;
+unsigned int i;
+unsigned int counter1 = 0;
+unsigned int counter2 = 1;
 
 if (s1 == NULL)
 	s1 = "";
@@ -23,22 +23,28 @@ if (s1 == NULL)
 if (s2 == NULL)
 	s2 = "";
 
-if (n >= strlen(s2))
-	counter = strlen(s2);
+for (i = 0; s1[i] != '\0'; i++)
+	counter1++;
+
+for (i = 0; s2[i] != '\0'; i++)
+	counter2++;
+
+if (n < counter2)
+	p = malloc(counter1 + n + 1);
+
 else
-	counter = n;
+	p = malloc(counter1 + counter2 + 1);
 
-ptr = malloc(strlen(s1) + counter + 1);
-
-if (ptr == NULL)
+if (p == NULL)
 	return (NULL);
 
-for (i = 0; s1[i] != '\0'; i++)
-	ptr[i] = s1[i];
+for (i = 0; i < counter1; i++)
+	p[i] = s1[i];
 
-for (i = 0; i < counter; i++)
-	ptr[i + strlen(s1)] = s2[i];
+for (i = 0; i < n; i++)
+	p[counter1 + i] = s2[i];
 
-ptr[i + strlen(s1) + 1] = '\0';
-return (ptr);
+p[counter1 + i + 1] = '\0';
+
+return (p);
 }
