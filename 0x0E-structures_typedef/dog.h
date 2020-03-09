5b8906489228eb57857bc051efdf6a9d7dbd2767
Eng-RonnieBM <ronniealberto@gmail.com>
2020-03-09 14:58:43 -0500

    creating task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - it defines a new type struct dog.
+ * @name: Dog's name.
+ * @age: Dog's age.
+ * @owner: Dog's owner.
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
