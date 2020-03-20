dded62ebb996c23e15fcd36d0d22d4c485bf41ae
Eng-RonnieBM <ronniealberto@gmail.com>
2020-03-19 23:04:57 -0500

    creating NULL condition

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,6 +16,8 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+		return (NULL);
 	i = 0;
 	while (new_node->str[i] != '\0')
 		i++;
