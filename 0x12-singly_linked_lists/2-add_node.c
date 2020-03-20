ac0609ffe655c19067b240288e347ebc6a5eaf0f
Eng-RonnieBM <ronniealberto@gmail.com>
2020-03-19 23:01:47 -0500

    testing corrections in code

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 
 /**
  * add_node - adds a new node at the beginning of a list_t list..
@@ -8,17 +9,19 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	int i = 0;
+	int i;
 	list_t *new_node;
 
 	new_node = (list_t *) malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
+	i = 0;
 	while (new_node->str[i] != '\0')
 		i++;
 	new_node->len = i;
 	new_node->next = *head;
+
 	*head = new_node;
 	return (new_node);
 }
