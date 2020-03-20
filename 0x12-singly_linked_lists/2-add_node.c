bc1afef2f2773fb7977f991e0d086b9077e1d441
Eng-RonnieBM <ronniealberto@gmail.com>
2020-03-19 22:43:25 -0500

    fixing code

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,16 +9,16 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	int i = 0;
-	list_t *new_node = (list_t *) malloc(sizeof(list_t));
+	list_t *new_node;
 
-	new_node->str = strdup((char *)str);
+	new_node = (list_t *) malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
+	new_node->str = strdup(str);
 	while (new_node->str[i] != '\0')
 		i++;
 	new_node->len = i;
 	new_node->next = *head;
-
 	*head = new_node;
 	return (new_node);
 }
