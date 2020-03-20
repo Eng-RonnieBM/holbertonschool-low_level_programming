14025d9c52400aa3953e92f6b13099fd94d5b450
Eng-RonnieBM <ronniealberto@gmail.com>
2020-03-19 23:27:42 -0500

    freeing mem

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,10 +14,16 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node = (list_t *) malloc(sizeof(list_t));
 	if (new_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
 	i = 0;
 	while (new_node->str[i] != '\0')
 		i++;
