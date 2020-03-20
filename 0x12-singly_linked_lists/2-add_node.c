2401dc05b4627d8a5e7785015dc9fde3ab0ac1c0
Eng-RonnieBM <ronniealberto@gmail.com>
2020-03-19 22:21:20 -0500

    fixing strdup

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,16 +4,17 @@
  * add_node - adds a new node at the beginning of a list_t list..
  * @head: the head.
  * @str: a string.
- * Return: starting node.
+ * Return: starting node in a linked list.
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	int i = 0;
+	char *_strcopy = strdup(str);
 	list_t *new_node = (list_t *) malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
-	new_node->str = (char *)str;
+	new_node->str = (char *)_strcopy;
 	while (new_node->str[i] != '\0')
 		i++;
 	new_node->len = i;
