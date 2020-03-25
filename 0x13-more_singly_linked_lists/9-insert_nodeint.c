d6c13fb471319cbb7da69210761f560f54c7902a
Eng-RonnieBM <ronniealberto@gmail.com>
2020-03-24 21:11:01 -0500

    fixing NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,8 +10,11 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 {
-	listint_t *copy, *new;
+	listint_t *copy;
+	listint_t *new;
 
+	if (*head == NULL)
+		return (NULL);
 	copy = *head;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
