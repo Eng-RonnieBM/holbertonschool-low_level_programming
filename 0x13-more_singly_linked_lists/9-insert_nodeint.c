2ad5d9dfea8dc7c93799fe7310ec45637bd81bb1
Eng-RonnieBM <ronniealberto@gmail.com>
2020-03-24 21:28:28 -0500

    fixing task9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,40 +3,40 @@
 /**
   * insert_nodeint_at_index - inserts a new node at a given position.
   * @head: head of the list.
-  * @index: index position to insert the node
+  * @idx: index position to insert the node
   * @n: value of the inserted node
   * Return: pointer to head of list
   */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *copy;
 	listint_t *new;
+	listint_t *temp = *head;
+	unsigned int i = 0;
 
-	if (*head == NULL)
-		return (NULL);
-	copy = *head;
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	if (idx == 0)
+	while (temp && i < idx - 1)
 	{
-		new->next = copy;
-		*head = new;
-		return (*head);
+		temp = temp->next;
+		i++;
 	}
-	while (idx > 1)
+	if (temp || (i == idx - 1 || idx == 0))
 	{
-		copy = copy->next;
-		idx--;
-		if (!copy)
+		new = malloc(sizeof(listint_t));
+		if (new)
 		{
-			free(new);
-			return (NULL);
+			new->n = n;
+			if (idx == 0)
+			{
+				new->next = *head;
+				*head = new;
+			}
+			else
+			{
+				new->next = temp->next;
+				temp->next = new;
+			}
+			return (new);
 		}
 	}
-	new->next = copy->next;
-	copy->next = new;
-	return (new);
+	return (NULL);
 }
