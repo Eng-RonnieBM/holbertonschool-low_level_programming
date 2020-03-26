340b780c7de8eaa378934cd2af3b61287aab8864
Eng-RonnieBM <ronniealberto@gmail.com>
2020-03-26 15:21:44 -0500

    fixing with shift bit manipul

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,52 +1,31 @@
 #include "holberton.h"
 
 /**
- * _pow - "a" power to the "b" number.
- * @a: base number.
- * @b: power of the base number.
- * Return: power of a number.
- */
-
-unsigned int _pow(unsigned int a, unsigned int b)
-{
-	unsigned int result = 1;
-
-	if (b == 0)
-		return (1);
-	while (b > 0)
-	{
-		result = result * a;
-		b--;
-	}
-	return (result);
-}
-
-/**
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b: pointer to a string.
  * Return: decimal conversion.
  */
-
 unsigned int binary_to_uint(const char *b)
 {
+	int counter = 0;
 	int i = 0;
-	unsigned int power = 0;
 	unsigned int sum = 0;
 
 	if (b == NULL)
 		return (0);
-	while (b[i + 1] != '\0')
+	while (b[counter] != '\0')
 	{
-		if (b[i] == '1' || b[i] == '0')
-			i++;
+		if (b[counter] == '1' || b[counter] == '0')
+			counter++;
 		else
-			return (sum);
+			return (0);
 	}
-	while (i >= 0)
+	while (i < counter)
 	{
-		sum = sum + (((unsigned int)b[i] - 48) * _pow(2, power));
-		power++;
-		i--;
+		sum = sum << 1;
+		if (b[i] == '1')
+			sum += 1;
+		i++;
 	}
-return (sum);
+	return (sum);
 }
