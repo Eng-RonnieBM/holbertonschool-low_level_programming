5d7c406d50ef7bb4b8b7d2d2cab3b08207c26c14
Eng-RonnieBM <ronniealberto@gmail.com>
2020-03-31 17:23:53 -0500

    fixing issues

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,27 +7,36 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buf;
 	ssize_t bytes;
 
-	/*if filename is NULL return 0.*/
-	if (filename == NULL)
-		return (0);
 	/*allocating space in memory*/
-	buf = malloc((sizeof(letters) + 1));
-	if (buf == NULL)
+	buf = malloc(sizeof(char) * letters + 1);
+	/*if buffer or filename = NULL, then return 0.*/
+	if (filename == NULL || buf == NULL)
 		return (0);
-	buf[(sizeof(letters) + 1)] = '\0';
 	/*opening the file READ ONLY*/
 	fd = open(filename, O_RDONLY);
 	/*if the file can not be opened or read, return 0*/
 	if (fd == -1)
+	{
+		free(buf);
+		close(fd);
 		return (0);
+
+	}
 	/*read the file*/
 	bytes = read(fd, buf, letters);
 	close(fd);
+	if (bytes == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
 	chars = write(2, buf, bytes);
 	free(buf);
 	/*if write fails or does not write the expected amount of bytes, return 0.*/
 	if (chars == -1)
 	{
+		free(buf);
 		return (0);
 	}
 	/*returns the actual number of letters it could read and print.*/
