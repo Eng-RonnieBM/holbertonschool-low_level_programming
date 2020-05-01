6ac18564eedeeefc5bdd1fb34d7559d9e77089d2
Eng-RonnieBM <ronniealberto@gmail.com>
2020-05-01 07:58:30 -0500

    fixing betty code'style

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef _LISTS_H_
-#def _LISTS_H_
+#define _LISTS_H_
 
 /**
  * struct dlistint_s - doubly linked list
@@ -12,9 +12,9 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
