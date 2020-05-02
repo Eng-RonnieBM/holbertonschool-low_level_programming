e56f851d27e7e2bed2adcec1ece910f1d116de4f
Eng-RonnieBM <ronniealberto@gmail.com>
2020-05-02 08:05:05 -0500

    fixing malloc Fail case

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,6 +15,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
 	*head = new;
 	new->n = n;
 	new->prev = NULL;
